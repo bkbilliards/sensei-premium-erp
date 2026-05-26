8cfba295e32529843034d2512376c1d973430950
bkbilliards <tobiktinec@gmail.com>
2026-05-26 15:24:53 +0500

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,21 +1,12 @@
-export const supabase = window.supabase.createClient(
-    'https://huryvmmweiyfgmumzxzh.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh1cnl2bW13ZWl5ZmdtdW16eHpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4MDc3MzAsImV4cCI6MjA5NDM4MzczMH0._kHBRzXqiQ16Lo1vz8xUeZnJBIv1bTpG5iX-3FZKhAg'
-);
+const SUPABASE_URL = 'https://huryvmmweiyfgmumzxzh.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh1cnl2bW13ZWl5ZmdtdW16eHpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4MDc3MzAsImV4cCI6MjA5NDM4MzczMH0._kHBRzXqiQ16Lo1vz8xUeZnJBIv1bTpG5iX-3FZKhAg';
+
 export const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
-export const session = {
-    isAuth: false,
-    user: null
-};
+export const session = { isAuth: false, user: null };
 
-export const saveSession = () => {
-    localStorage.setItem('sensei_session', JSON.stringify(session));
-};
-
+export const saveSession = () => localStorage.setItem('sensei_session', JSON.stringify(session));
 export const loadSession = () => {
     const s = localStorage.getItem('sensei_session');
-    if (s) {
-        Object.assign(session, JSON.parse(s));
-    }
+    if (s) Object.assign(session, JSON.parse(s));
 };
