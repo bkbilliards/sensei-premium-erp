59bdfe57ff26f54588c19db5de95752142206ad4
bkbilliards <tobiktinec@gmail.com>
2026-05-26 15:21:15 +0500

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -2,18 +2,20 @@ export const supabase = window.supabase.createClient(
     'https://huryvmmweiyfgmumzxzh.supabase.co',
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh1cnl2bW13ZWl5ZmdtdW16eHpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4MDc3MzAsImV4cCI6MjA5NDM4MzczMH0._kHBRzXqiQ16Lo1vz8xUeZnJBIv1bTpG5iX-3FZKhAg'
 );
+export const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
-export let session = {
+export const session = {
     isAuth: false,
-    user: null,
-    activeAdminName: null
+    user: null
 };
 
-export function saveSession() {
-    localStorage.setItem('sensei_session_x', JSON.stringify(session));
-}
+export const saveSession = () => {
+    localStorage.setItem('sensei_session', JSON.stringify(session));
+};
 
-export function loadSession() {
-    const s = localStorage.getItem('sensei_session_x');
-    if (s) Object.assign(session, JSON.parse(s));
-}
+export const loadSession = () => {
+    const s = localStorage.getItem('sensei_session');
+    if (s) {
+        Object.assign(session, JSON.parse(s));
+    }
+};
